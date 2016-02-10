dc76832243c946fe2d3eb069e72c90d72f4487ed
Krystan Menne and Nathan Romike <kmenne@gmail.com>
2016-02-10 09:53:40 -0800

    add board methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,45 @@
+function Board() {
+  this.spaces = this.generateBoard();
+}
+
+Board.prototype.find = function (xCoordinate, yCoordinate) {
+  // for each space in spaces,
+  // check if that space's coordinates match the test coordinates
+  // if so, return that space
+
+  var spaces = this.spaces;
+  for (var i = 0; i < spaces.length; i++) {
+    var space = spaces[i]
+    if (space.xCoordinate === xCoordinate && space.yCoordinate === yCoordinate) {
+      return space;
+    }
+  }
+};
+
+Board.prototype.generateBoard = function () {
+  var spaces = []
+  for (var x = 0; x < 3; x++) {
+    for (var y = 0; y < 3; y++) {
+      spaces.push(new Space(x, y));
+    }
+  }
+  return spaces;
+};
+
 function Player(mark) {
   this.mark = mark;
 }
 
-function Space(xCoordinate, yCoordinate) {
+function Space(xCoordinate, yCoordinate, optionalMark) {
   this.xCoordinate = xCoordinate;
-
+  this.yCoordinate = yCoordinate;
+  this.coordinates = [xCoordinate, yCoordinate];
+  this.markedBy;
 }
+
+Space.prototype.markedBy = function(player) {
+  if (player) {
+    this.markedBy = player;
+  }
+  return this.markedBy;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('Player', function() {
   it("returns the player's mark", function() {
-    var testPlayer = new Player("X");
+    var testPlayer = createXPlayer();
     expect(testPlayer.mark).to.equal("X");
   });
 });
@@ -10,4 +10,41 @@ describe('Space', function() {
     var testSpace = new Space(1, 2);
     expect(testSpace.xCoordinate).to.equal(1);
   });
+
+  it('returns y-coordinate', function() {
+    var testSpace = new Space(1, 2);
+    expect(testSpace.yCoordinate).to.equal(2);
+  });
+
+  it('lets a player mark a space', function() {
+    var testPlayer = createXPlayer();
+    var testSpace = createTestSpace();
+    testSpace.markedBy(testPlayer);
+    expect(testSpace.markedBy).to.equal(testPlayer);
+  });
 });
+
+describe('Board', function() {
+  it('creates 9 spaces when it is initialized', function() {
+    var testBoard = new Board();
+    expect(testBoard.spaces.length).to.equal(9);
+    expect(testBoard.spaces[0].coordinates).to.eql([0,0]);
+    expect(testBoard.spaces[8].coordinates).to.eql([2,2]);
+  });
+
+  describe('.find()', function() {
+    it('finds a space within the board by its coordinates', function() {
+      var testBoard = new Board();
+      expect(testBoard.find(0,0).coordinates).to.eql([0,0]);
+      expect(testBoard.find(1,2).coordinates).to.eql([1,2]);
+    });
+  });
+});
+
+var createXPlayer = function() {
+  return new Player("X");
+};
+
+var createTestSpace = function() {
+  return new Space(1, 2);
+};
